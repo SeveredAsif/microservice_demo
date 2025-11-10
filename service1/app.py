b29bc11fec7b552c9102fdc1a29793372f2f7123
Ali Asif Khan <aritra.khan91@gmail.com>
2025-11-10 11:13:49 +0600

    first commit

---
+++ service1/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify(message="Hello from Service 1!")
+
+@app.route('/call-service2')
+def call_service2():
+    try:
+        response = requests.get("http://service2:5001/")
+        return jsonify(message="Service 1 received: " + response.json()['message'])
+    except Exception as e:
+        return jsonify(error=str(e)), 500
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
