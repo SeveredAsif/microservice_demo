cbd7498df3b71ba86e8571a3931e187bfbb4b500
Ali Asif Khan <aritra.khan91@gmail.com>
2025-11-10 13:14:47 +0600

    updated deployed URL

--- service1/app.py
+++ service1/app.py
@@ -10,7 +10,8 @@ def home():
 @app.route('/call-service2')
 def call_service2():
     try:
-        response = requests.get("http://service2:5001/")
+        #response = requests.get("http://service2:5001/")
+        response = requests.get("https://microservice-demo-2new.onrender.com/")
         return jsonify(message="(CI CHECK PUSH) Service 1 received: " + response.json()['message'])
     except Exception as e:
         return jsonify(error=str(e)), 500
